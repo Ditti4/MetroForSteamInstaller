abdd668c5efd27fa6853e267ea2ee0c95d1e8119
Ditti4 <ricod1996@gmail.com>
2014-08-16 16:20:02 +0200

    Added ability to download the zip file from deviantart and save it to the disk. Also added some comments.

--- MetroInstaller/mainwindow.vb
+++ MetroInstaller/mainwindow.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 Imports System.Text.RegularExpressions
 Imports System.Net
+Imports System.Text
 
 Public Class mainwindow
 
@@ -11,10 +12,7 @@ Public Class mainwindow
     Dim filestring As String
     Dim first As Integer
     Dim length As Integer
-
-    Private Sub updatebutton_Click(sender As Object, e As EventArgs) Handles updatebutton.Click
-        updatenotice.Text = "Wurkz"
-    End Sub
+    Dim cookies As New CookieContainer
 
     Private Sub mainwindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         '
@@ -29,7 +27,6 @@ Public Class mainwindow
         If String.IsNullOrEmpty(installpath) Then
             MessageBox.Show("Looks like Steam is not installed. This installer is useless for you then.", "Oops!", MessageBoxButtons.OK)
         End If
-
         '
         'Get the currently installed Metro for Steam version from resource\menus\steam.menu
         '
@@ -40,25 +37,49 @@ Public Class mainwindow
             currentversion = filestring.Substring(first, length)
         Else
             currentversion = "Not installed (yet?)"
+            '
+            'Make sure the custom.styles file gets copied if the skin isn't installed yet
+            '
+            overwritecheckbox.Checked = True
         End If
         currentversionlabel.Text = currentversion
 
         Try
+            '
+            'Get Metro for Steam deviantart page
+            '
             Dim req As HttpWebRequest = WebRequest.Create("http://boneyardbrew.deviantart.com/art/MfS-292419787")
             req.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
-            Dim stream = req.GetResponse.GetResponseStream
+            req.CookieContainer = cookies
+            Dim resp As HttpWebResponse = req.GetResponse
+            Dim stream = resp.GetResponseStream
             Dim reader = New System.IO.StreamReader(stream)
             filestring = reader.ReadToEnd
             stream.Close()
             reader.Close()
+            '
+            'Get newversion from deviantart
+            '
             first = filestring.IndexOf("Metro for Steam - ") + 18
             length = filestring.IndexOf(" by Bone", first) - first
             newversion = filestring.Substring(first, length)
             newversionlabel.Text = newversion
+            '
+            'Get download link from deviantart
+            '
+            first = filestring.IndexOf("http://www.deviantart.com/download/")
+            length = filestring.IndexOf("""", first) - first
+            dlurl = filestring.Substring(first, length).Replace("&amp;", "&")
+            '
+            'Get cookies from previous response
+            '
+            cookies.Add(resp.Cookies)
         Catch exc As WebException
             MessageBox.Show(exc.Message + exc.ToString, "Oops!", MessageBoxButtons.OK)
         End Try
-
+        '
+        'Compare currentversion and newversion, give advice afterwards
+        '
         If newversion.Equals(currentversion) Then
             updatenotice.Text = "All good, you alreday have the newest version."
             updatenotice.ForeColor = System.Drawing.Color.FromArgb(130, 186, 0)
@@ -67,5 +88,33 @@ Public Class mainwindow
             updatenotice.ForeColor = System.Drawing.Color.FromArgb(93, 178, 255)
         End If
 
+    End Sub
+
+    Private Sub updatebutton_Click(sender As Object, e As EventArgs) Handles updatebutton.Click
+        Try
+            '
+            'Status update so the user knows the app didn't crash
+            '
+            updatenotice.Text = "Working..."
+            updatenotice.ForeColor = System.Drawing.Color.FromArgb(93, 178, 255)
+            '
+            'Let's form the request for the zip download, get the file and save it to the disk
+            '
+            Dim req As HttpWebRequest = HttpWebRequest.Create(dlurl)
+            req.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
+            req.Referer = "http://boneyardbrew.deviantart.com/art/MfS-292419787"
+            req.CookieContainer = cookies
+            Dim stream = req.GetResponse.GetResponseStream
+            Dim filestream = File.Create("mfs.zip")
+            stream.CopyTo(filestream)
+            filestream.Close()
+            stream.Close()
+            '
+            'Next: unzipping
+            '
+
+        Catch exc As WebException
+            MessageBox.Show(exc.Message + exc.ToString, "Oops!", MessageBoxButtons.OK)
+        End Try
     End Sub
 End Class
